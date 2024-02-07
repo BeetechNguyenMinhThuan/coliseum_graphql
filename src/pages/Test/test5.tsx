1d4f42a4a1f0056993e15740a0542f7af78e4a7d
Beetech-DuyMD <duy.md@beetechsoft.vn>
2024-02-07 17:30:12 +0700

    duyy

--- src/pages/Test/test5.tsx
+++ src/pages/Test/test5.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./test5.scss";
 import classNames from "@/utils/classNames";
 
@@ -7,7 +7,7 @@ const rounds = [
     {
       name: "round 1",
       a: {
-        name: "",
+        name: "【四方世界へ】世界中を旅したバカだけど何か質問がありますか？ [さあ行こう]",
         vote: "",
       },
       b: {
@@ -275,7 +275,7 @@ const rounds = [
     {
       name: "round 1",
       a: {
-        name: "",
+        name: "ádasdasdasdasádasdas dasdasádasdasdasdasádasdasdasdasádasdas dasdasádasdasdasdasádasdasdasdasádasdas dasdasádasdasdasdasádasdasdasdasádasdas dasdasádasdasdasdasádasdasdasdasádasdas dasdasádasdasdasdasádasdasdasdasádasdas dasdasádasdasdasdas",
         vote: "",
       },
       b: {
@@ -354,6 +354,15 @@ const rounds = [
       },
     },
   ],
+  [
+    {
+      name: "champion",
+      a: {
+        name: "123",
+        vote: "",
+      },
+    },
+  ],
 ];
 
 console.log(rounds);
@@ -362,7 +371,7 @@ export default function Test5() {
     <div className="abcc w-full ">
       {rounds.map((round, roundIndex) => (
         <div
-          className={`flex flex-col justify-center column-${roundIndex + 1}  `}
+          className={`flex flex-col column-${roundIndex + 1}  `}
           key={`round-${roundIndex}`}
         >
           {round.map((match, matchIndex) => (
@@ -370,39 +379,54 @@ export default function Test5() {
               className={` roundd relative round-${roundIndex + 1}  flex flex-1 flex-col `}
               key={`match-${matchIndex}`}
             >
-              <div className=" relative flex flex-1 items-center">
-                <div className=" w-full border-2">
-                  {match.a.name} - {match.a.vote}
-                </div>
-
-                <div
-                  className={classNames(
-                    `line absolute left-full top-1/2 h-0.5  w-[150px]`,
-                    match.a.vote > match.b.vote ? "bg-red-500" : "bg-black",
+              {roundIndex + 1 == rounds.length ? (
+                <Fragment>
+                   
+                <div className=" relative flex flex-1 items-center w-[300px] ">
+                    <div className="w-full relative">
+                    <div className="absolute bottom-10">CHAMPION</div>
+                    <div className=" w-full border-2 ">
+                    {match.a.name} - {match.a.vote}
+                  </div>
+                    </div>
+                  
+                  {roundIndex > 0 && (
+                    <div className=" line-sub absolute right-full top-1/2 h-0.5  w-[60px] bg-red-500"></div>
                   )}
-                ></div>
-                {roundIndex > 0 && (
-                  <div className=" line-sub absolute right-full top-1/2 h-0.5  w-[60px] bg-red-500"></div>
-                )}
-              </div>
-
-              <div className=" relative flex flex-1 items-center">
-                <div className=" w-full  border-2">
-                  {match.b.name} - {match.b.vote}
                 </div>
-                <div
-                  className={`line absolute left-full top-1/2 h-0.5  w-[150px] bg-black`}
-                ></div>
+                </Fragment>
+              ) : (
+                <div className=" relative flex flex-1 items-center after:w-[2px] w-[300px]">
+                  <div className=" w-full border-2">
+                    {match.a.name} - {match.a.vote}
+                  </div>
 
-                {roundIndex > 0 && (
                   <div
-                    className=" line-sub absolute right-full top-1/2 h-0.5  w-[60px]  bg-red-500"
-                    style={{
-                      transform: "",
-                    }}
+                    className={classNames(
+                      `line absolute left-full top-1/2 h-0.5  w-[150px]`,
+                      match.a.vote > match?.b?.vote ? "bg-red-500" : "bg-black",
+                    )}
                   ></div>
-                )}
-              </div>
+                  {roundIndex > 0 && (
+                    <div className=" line-sub absolute right-full top-1/2 h-0.5  w-[60px] bg-red-500"></div>
+                  )}
+                </div>
+              )}
+
+              {roundIndex !== 5 && (
+                <div className=" relative flex flex-1 items-center after:w-[2px] w-[300px]">
+                  <div className=" w-full  border-2">
+                    {match?.b?.name} - {match?.b?.vote}
+                  </div>
+                  <div
+                    className={`line absolute left-full top-1/2 h-0.5  w-[150px] bg-black`}
+                  ></div>
+
+                  {roundIndex > 0 && (
+                    <div className=" line-sub absolute right-full top-1/2 h-0.5  w-[60px]  bg-red-500"></div>
+                  )}
+                </div>
+              )}
             </div>
           ))}
         </div>
